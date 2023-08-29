8c379a60bbebb256b3aef4ace0791000bbf054e2
mariavjs <mariavjs@al.insper.edu.br>
2023-08-29 07:55:19 -0300

    upload documentos

---
+++ utils.py
@@ -0,0 +1,37 @@
+def extract_route(request):
+    barra = request.find("/")
+    http = request.find("HTTP")
+
+    route = request[barra+1:http-1]
+
+    return route
+
+def read_file(path):
+    with open(path, "rb") as arquivo:
+        texto = arquivo.read()
+
+    
+    return texto
+
+import json
+
+def load_data(arq):
+    path = "data/{nome}".format(nome=arq)
+    with open(path, "r") as arquivo:
+
+        dict = json.load(arquivo)
+
+    return dict
+
+
+def load_template(template):
+    path = "templates/{template}".format(template=template)
+    with open(path, "r") as arquivo:
+
+        conteudo = arquivo.read()
+
+    return conteudo
+
+def build_response(body='', code=200, reason='OK', headers=''):
+    resposta = "HTTP/1.1 {code} {reason}\n{headers}\n\n{body}" .format(code=code, reason=reason, headers=headers, body=body)
+    return resposta.encode()
